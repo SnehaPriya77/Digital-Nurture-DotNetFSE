904fe2a091124345e4dda59224ceae3c192b11b0
SnehaPriya77 <sneha.valmiki77@gmail.com>
2026-06-23 19:04:47 +0530

    Add files via upload

---
+++ DeepSkilling/Module1- Design Patterns and Principles/MVCPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace MVCPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student(101, "Sneha", "B");
+
+            StudentView view = new StudentView();
+
+            StudentController controller =
+                new StudentController(student, view);
+
+            controller.UpdateView();
+
+            controller.SetStudentName("Priya");
+            controller.SetStudentGrade("A+");
+
+            System.Console.WriteLine("\nAfter Update:\n");
+
+            controller.UpdateView();
+        }
+    }
+}
